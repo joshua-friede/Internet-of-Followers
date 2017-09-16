0d6cdfebfd3475b13e544d26026272c81286bc71
Joshua Friede <joshua-friede@users.noreply.github.com>
2017-09-16 12:25:57 -0500

    add base code and gitignore

---
+++ IoF.py
@@ -0,0 +1,13 @@
+from TwitterAPI import TwitterAPI
+
+
+consumer_key = ""
+consumer_secret = ""
+access_token_key = ""
+access_token_secret = ""
+
+api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
+
+def foo():
+    print("hello world")
+    
